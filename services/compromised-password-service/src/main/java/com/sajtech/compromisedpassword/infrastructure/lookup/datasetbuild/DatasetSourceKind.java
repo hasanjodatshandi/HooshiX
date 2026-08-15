eb10bb5d93dcf0b316a0275e6c4e50d12274ba23
hasan.jodat.shandi <hasan.jodat.shandi@gmail.com>
2026-08-15 17:27:01 +0330

    Model dataset source evidence explicitly

---
+++ services/compromised-password-service/src/main/java/com/sajtech/compromisedpassword/infrastructure/lookup/datasetbuild/DatasetSourceKind.java
@@ -0,0 +1,6 @@
+package com.sajtech.compromisedpassword.infrastructure.lookup.datasetbuild;
+
+public enum DatasetSourceKind {
+  GENERATED_TEST_FIXTURE,
+  HIBP_PWNED_PASSWORDS_COMPLETE_DOWNLOAD
+}
