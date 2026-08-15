f8d45ad9b9659bb0c3e013b17fd224810df3d930
hasan.jodat.shandi <hasan.jodat.shandi@gmail.com>
2026-08-15 20:11:37 +0330

    Remove infrastructure-owned lookup failure types

--- services/compromised-password-service/src/main/java/com/sajtech/compromisedpassword/infrastructure/lookup/runtime/LookupCapacityExceededException.java
+++
@@ -1,7 +0,0 @@
-package com.sajtech.compromisedpassword.infrastructure.lookup.runtime;
-
-public final class LookupCapacityExceededException extends RuntimeException {
-  public LookupCapacityExceededException() {
-    super("Lookup capacity is exhausted");
-  }
-}
